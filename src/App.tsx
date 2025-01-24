babae34b8adfd0f7ffc57e3e637f9fb99b420137
Jason Athanasoglou <jathanasoglou@outlook.com>
2025-01-24 23:58:12 +0200

    Add mui, roboto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import "@/App.css";
 import { useMupdf } from "@/hooks/useMupdf.hook";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { useEffect, useState } from "react";
 
 function App() {
